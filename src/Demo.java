2fe9d48e216973e31e1061b0f832981752ad51f6
liupu <575866447@qq.com>
2019-05-03 10:31:04 +0800

    rename_class

--- src/Demo.java
+++ src/Demo.java
@@ -6,14 +6,14 @@ import java.io.IOException;
 public class Demo {
 
     public static void main(String[] args) throws IOException {
-        ClassReader cr = new ClassReader(User.class.getName());
+        ClassReader cr = new ClassReader(Hello.class.getName());
         ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
         ClassVisitor cv = new TestClassVisitor(cw);
         cr.accept(cv, Opcodes.ASM5);
         // 获取生成的class文件对应的二进制流
         byte[] code = cw.toByteArray();
         //将二进制流写到out/下
-        FileOutputStream fos = new FileOutputStream("out/User.class");
+        FileOutputStream fos = new FileOutputStream("out/Hello.class");
         fos.write(code);
         fos.close();
     }
--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,4 @@
-class User {
+class Hello {
     public static void main(String[] args) {
         show();
     }
